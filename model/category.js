51e53462ac71f65ccfa29a98f012ce0717e68887
naknamu <jodeldelvalle@gmail.com>
2023-04-25 16:53:51 +0800

    Added category model

---
+++ model/category.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    blogPost: {
+        type: Schema.Types.ObjectId,
+        ref: 'BlogPost',
+        required: true
+    }
+});
+
+// Virtual for blogPost's URL
+categorySchema.virtual("url").get(function () {
+    // We don't use an arrow function as we'll need the this object
+    return `/api/categories/${this._id}`;
+});
+
+module.exports = mongoose.model('Category', categorySchema);
