c437721a51836158eaa4ad4ed1392c37e311624b
naknamu <jodeldelvalle@gmail.com>
2023-04-25 16:57:02 +0800

    Added tag model

---
+++ model/tag.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tagSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    blogPost: {
+        type: Schema.Types.ObjectId,
+        ref: 'BlogPost',
+        required: true
+    }
+});
+
+// Virtual for blogPost's URL
+tagSchema.virtual("url").get(function () {
+    // We don't use an arrow function as we'll need the this object
+    return `/api/tags/${this._id}`;
+});
+
+module.exports = mongoose.model('Tag', tagSchema);
