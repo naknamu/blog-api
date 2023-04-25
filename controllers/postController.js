4036f7d16f40e1517101b2b89b70cfb205ee2319
naknamu <jodeldelvalle@gmail.com>
2023-04-25 18:47:54 +0800

    Added controller for blog post

---
+++ controllers/postController.js
@@ -0,0 +1,54 @@
+const Post = require("../model/post");
+const asyncHandler = require('express-async-handler')
+
+// Display list of all Blog Posts
+blogPost_list = asyncHandler( async(req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post LIST")
+})
+
+// Display detail page for a specific Blog Post.
+blogPost_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Blog Post detail: ${req.params.id}`);
+});
+
+// Display Blog Post create form on GET.
+blogPost_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post create GET");
+});
+
+// Handle Blog Post create on POST.
+blogPost_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post create POST");
+});
+
+// Display Blog Post delete form on GET.
+blogPost_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post delete GET");
+});
+
+// Handle Blog Post delete on POST.
+blogPost_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post delete POST");
+});
+
+// Display Blog Post update form on GET.
+blogPost_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post update GET");
+});
+
+// Handle Blog Post update on POST.
+blogPost_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Blog Post update POST");
+});
+
+module.exports = {
+    blogPost_list,
+    blogPost_detail,
+    blogPost_create_get,
+    blogPost_create_post,
+    blogPost_delete_get,
+    blogPost_delete_post,
+    blogPost_update_get,
+    blogPost_update_post
+}
+
