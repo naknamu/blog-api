789fd9fcd17003803e49480bbfabc5f989d5e5ab
naknamu <jodeldelvalle@gmail.com>
2023-04-26 17:37:26 +0800

    Added a script to send json in homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,29 @@
+const Post = require("../model/post");
+const Category = require("../model/category");
+const Tag = require("../model/tag");
+
 const asyncHandler = require('express-async-handler')
 
 // SITE HOMEPAGE
 home = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+    // Get details of published blog posts, categories and tags (in parallel)
+    const [
+        blogPosts,
+        categories,
+        tags
+    ] =  await Promise.all([
+        Post.find({published: true}).sort({publishedDate: -1}).exec(),
+        Category.find({}).sort({name: 1}).exec(),
+        Tag.find({}).sort({name: 1}).exec()
+    ])
+
+    const data = {
+        blogPosts,
+        categories,
+        tags
+    }
+
+    res.json(data);
 });
 
 module.exports = {home};
