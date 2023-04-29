3958c6bd3a4d4e9c09e83150c2efecbb1ff5baeb
naknamu <jodeldelvalle@gmail.com>
2023-04-29 08:28:06 +0800

    Added error handler function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const createError = require("http-errors");
 const apiRouter = require('./routes/api');
 
 require('dotenv').config();
@@ -36,6 +37,22 @@ app.use(express.urlencoded({ extended: true }));
 
 // Define routes
 app.use("/api", apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
