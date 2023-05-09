c06e66ecc4644b2ecf928a9d4900ebecee7dec63
naknamu <jodeldelvalle@gmail.com>
2023-05-09 14:31:02 +0800

    Added comment and user

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,22 +1,28 @@
 const Post = require("../model/post");
 const Category = require("../model/category");
 const Tag = require("../model/tag");
+const Comment = require("../model/comment");
+const User = require("../model/user");
 
 const asyncHandler = require("express-async-handler");
 
 // SITE HOMEPAGE
 home = asyncHandler(async (req, res, next) => {
   // Get details of published blog posts, categories and tags (in parallel)
-  const [blogPosts, categories, tags] = await Promise.all([
+  const [blogPosts, categories, tags, comments, users] = await Promise.all([
     Post.countDocuments().exec(),
     Category.countDocuments().exec(),
     Tag.countDocuments().exec(),
+    Comment.countDocuments().exec(),
+    User.countDocuments().exec(),
   ]);
 
   const data = {
     blog_count: blogPosts,
     category_count: categories,
     tag_count: tags,
+    comment_count: comments,
+    user_count: users,
   };
 
   res.status(200).json(data);
