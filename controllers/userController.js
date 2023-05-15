6fad0e5be96867dbd06c367fd41e5da820124a6f
naknamu <jodeldelvalle@gmail.com>
2023-05-15 15:28:43 +0800

    Added user create function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
 
 const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, {expiresIn: '7d'});
@@ -21,4 +23,37 @@ const loginUser = async (req, res) => {
     }
 }
 
-module.exports = { loginUser };
+// Create a user
+const createUser = [
+
+    // Validate and sanitize fields.
+    body("email", "Email must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .isEmail()
+    .withMessage("Must be an email"),
+    body("password", "Password must not be empty.")
+    .trim()
+    .isLength({ min: 1 }),
+
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+    
+        // Create a Book object with escaped and trimmed data.
+        const user = new User({
+          email: req.body.email,
+          password: req.body.password,
+        });
+    
+        if (!errors.isEmpty()) {
+          res.status(400).json(errors.mapped());
+        } else {
+          await user.save();
+          res.status(200).json({ message: `Successfully saved user: ${req.body.email}` });
+        }
+    }),
+]
+
+
+module.exports = { loginUser, createUser };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { loginUser } = require("../controllers/userController");
+const { loginUser, createUser } = require("../controllers/userController");
 
 router.post('/login', loginUser);
+
+router.post('/create', createUser);
 
 module.exports = router;
