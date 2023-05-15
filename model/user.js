59c59bbfd546a44b4cc3851b1a4c355c13bbf3fb
naknamu <jodeldelvalle@gmail.com>
2023-05-15 09:05:49 +0800

    Added static login method

--- model/user.js
+++ model/user.js
@@ -27,10 +27,24 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.checkPassword = async function (password) {
-  const user = this;
-  const result = await bcrypt.compare(password, user.password);
-  return result;
-};
+// static login method
+userSchema.statics.login = async function(email, password) {
+
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
 
 module.exports = mongoose.model("User", userSchema);
